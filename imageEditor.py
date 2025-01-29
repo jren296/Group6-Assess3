c1cfb9a4e32ccab85030c035b91dead55b3a18f5
jren296 <josh.rendell@outlook.com>
2025-01-29 22:38:26 +1100

    added load_image, display_image, start_crop, update_crop, finish_crop definitions

--- imageEditor.py
+++ imageEditor.py
@@ -44,7 +44,57 @@ class ImageEditor:
         self.root.bind('<Control-s', lambda event: self.save_image())
 
     def load_image(self):
-        
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpeg *.jpg *.png *.bmp")])
+        if file_path:
+            self.image = cv2.imread(file_path)
+            self.display_image(self.image)
+            self.saveButton.config(state=tk.DISABLED)
+            self.undo_stack.clear()
+            self.redo_stack.clear()
+            self.undoButton.config(state=tk.DISABLED)
+            self.redoButton.config(state=tk.DISABLED)
 
+    def display_image(self, image):
+        self.thumbnail = cv2.resize(image, (1000, 800), interpolation=cv2.INTER_AREA)
+        thumbnail_pil = Image.fromarray(cv2.cvtColor(self.thumbnail, cv2.COLOR_BGR2RGB))
+        thumbnail_tk = ImageTk.PhotoImage(thumbnail_pil)
+        self.canvas.create_image(500, 400, image=thumbnail_tk, anchor="center")
+        self.canvas.image = thumbnail_tk # Keeps a reference image
 
+    def start_crop(self, event):
+        if self.image is not None:
+            self.rect_start = (event.x, event.y)
 
+    def update_crop(self, event):
+        if self.image is not None and self.rect_start:
+            self.canvas.delete("crop_rect")
+            self.rect_end = (event.x, event.y)
+            self.canvas.create_rectangle(self.rect_start[0], self.rect_start[1], self.rect_end[0], self.rect_end[1],
+                                         outline='blue', tag='crop_rect')
+            
+    def finish_crop(self, event):
+        if self.image is not None and self.rect_start and self.rect_end:
+            x1, y1 = self.rect_start
+            x2, y2 = self.rect_end
+            x1, x2 = sorted([x1, x2])
+            y1, y2 = sorted([y1, y2])
+            h, w, _ = self.image.shape
+            scale_x = self.image.shape[1]/ 1000
+            scale_y = self.image.shape[0]/ 800
+            # Map canvas coords to image coords
+            x1 = int(x1 * scale_x)
+            x2 = int(x2 * scale_x)
+            y1 = int(y1 * scale_y)
+            y2 = int(y2 * scale_y)
+            # Crop and display image
+            self.cropped_image = self.image[y1:y2, x1:x2]
+            self.displayed_cropped = self.cropped_image.copy()
+            self.display_image()
+            # Set up undo and save
+            self.undo_stack.append(self.image.copy())
+            self.redo_stack.clear()
+            self.undoButton.config(state=tk.NORMAL)
+            self.redoButton.config(state=tk.DISABLED)
+            self.saveButton.config(state=tk.NORMAL)
+
+            
